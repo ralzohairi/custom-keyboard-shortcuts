cce8d00a055225cb76cfc1c61009a2bc21f11b2b
Reema <reema.en@gmail.com>
2022-12-11 11:37:10 +0300

    modify code for getting started example

--- custom-keyboard-shortcuts.js
+++ custom-keyboard-shortcuts.js
@@ -2,47 +2,17 @@
 avoided as MDN considers it a deprecated attribute. Therefore, the key 
 attribute is used instead.*/
 
+
+// 1) Keep track of clicked keys
+
+// 2) Avoid keeping track of the ctrl key, the shift key and the alt key
+
+// 3) Check whether the desired custom shortcut is clicked
+
+
 const shortcutNotClickedTextRef = document.getElementsByClassName("shortcut-not-clicked-text")[0];
 const shortcutClickedTextRef = document.getElementsByClassName("shortcut-clicked-text")[0];
 
-// Keep track of clicked keys
-var isKeyPressed = {
-    'a': false, // ASCII code for 'a'
-    'b': false, // ASCII code for 'b'
-    // ... Other keys to check for custom key combinations
-};
-
-document.onkeydown = (keyDownEvent) => {
-
-    //Prevent default key actions, if desired
-    keyDownEvent.preventDefault();
-
-    // Track down key click
-    isKeyPressed[keyDownEvent.key] = true;
-
-    // Check described custom shortcut
-    if (isKeyPressed["a"] && isKeyPressed["b"]) { //for example we want to check if a and b are clicked at the same time
-        //do something as custom shortcut (a & b) is clicked
-
-        // show text indicating shortcut is clicked
-        displayShortcutClickedText();
-    };
-}
-
-document.onkeyup = (keyUpEvent) => {
-
-    // Prevent default key actions, if desired
-    keyUpEvent.preventDefault();
-
-    // Track down key release
-    isKeyPressed[keyUpEvent.key] = false;
-
-    // when one of the keys is released, show text indicating
-    // text is no longer clicked
-    if (!isKeyPressed["a"] || !isKeyPressed["b"]) {
-        hideShortcutClickedText();
-    };
-};
 
 function displayShortcutClickedText() {
     shortcutClickedTextRef.style.display = "flex";
